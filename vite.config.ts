c642b0e623f36d746338b8a91004e38c3ab0fcd0
bhaveshadtani <bhaveshadtani2001@gmail.com>
2025-08-11 15:48:11 +0530

    Add deploy script and alias config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/bhavesh-portfolio/", // 👈 your GitHub repo name here
   server: {
     host: "::",
     port: 8080,
